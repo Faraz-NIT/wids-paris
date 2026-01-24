4cdcbe240f78f992f75936456e5006e944304474
Faraz <farazgudlife2@gmail.com>
2026-01-24 21:12:33 +0100

    text

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ function App() {
         <div className="schedule-cards-row">
           <div className="schedule-card">
             <div className="speaker-avatar-circle" style={{backgroundImage: 'url(/images/justine.jpg)'}}></div>
-            <h3>2ème conférence</h3>
+            
             <p className="speaker-name">Justine Nerce</p>
             <p className="speaker-role">CEO at ARTEFACT</p>
             <div className="schedule-card-hover">
@@ -113,7 +113,7 @@ function App() {
         <div className="schedule-cards-row">
            <div className="schedule-card">
             <div className="speaker-avatar-circle" style={{backgroundImage: 'url(/images/jacqueline.jpg)'}}></div>
-            <h3>2ème conférence</h3>
+           
             <p className="speaker-name">Jacqueline Lefevre Lopez</p>
             <p className="speaker-role">Senior Data Scientist at UBISOFT</p>
             <div className="schedule-card-hover">
@@ -125,7 +125,7 @@ function App() {
           </div>
           <div className="schedule-card">
             <div className="speaker-avatar-circle" style={{backgroundImage: 'url(/images/cristina.jpg)'}}></div>
-            <h3>2ème conférence</h3>
+            
             <p className="speaker-name">Cristina Oprean</p>
             <p className="speaker-role">Head of AI, Governance at AXA</p>
             <div className="schedule-card-hover">
@@ -137,7 +137,7 @@ function App() {
           </div>
           <div className="schedule-card">
             <div className="speaker-avatar-circle" style={{backgroundImage: 'url(/images/marion.jpg)'}}></div>
-            <h3>Table ronde</h3>
+          
             <p className="speaker-name">Marion JULLIEN Ségolène</p>
             <p className="speaker-role">Insights & Data Leader, Global Tech Lead Governance and Responsible AI at 3DS</p>
             <div className="schedule-card-hover">
@@ -157,9 +157,15 @@ function App() {
         <div className="schedule-cards-row">
           <div className="schedule-card">
             <div className="speaker-avatar-circle" style={{backgroundImage: 'url(/images/dessertine.jpg)'}}></div>
-            <h3>Table ronde</h3>
+            
             <p className="speaker-name">Dessertine-Panhard</p>
             <p className="speaker-role">Corporate Strategic Planning - Strategic Client at AWS</p>
+            <div className="schedule-card-hover">
+    <h3>About</h3>
+              <p className="hover-text">
+                As CMO of SFR Business, she drives product strategy and growth across enterprise and SMB segments, leading digital marketing, data, and BI to deliver innovative, sustainable solutions. With 15+ years in telecom, she has led global initiatives across Mobile, Broadband, Cybersecurity, and IoT, fostering collaboration and innovation across regions
+              </p>
+  </div>
           </div>
         </div>
       </div>
@@ -185,7 +191,17 @@ function App() {
   <div className="container">
     <div className="footer-content">
       <div className="footer-section">
-        <h4>About WiDS Global</h4>
+        <h3>
+  <a
+    href="https://www.widsconference.org"
+    target="_blank"
+    rel="noopener noreferrer"
+    
+  >
+    About WiDS Global
+  </a>
+</h3>
+
         <p>
           Empowering women in data science through education, community, and
           opportunity.
@@ -194,12 +210,18 @@ function App() {
 
       <div className="footer-section">
         
-        
+        <h3>Quick Links</h3>
+        <ul className="footer-links">
+          
+          <li><a href="#agenda-section">Schedule</a></li>
+          <li><a href="#schedule">Speakers</a></li>
+          <li><a href="#register">Register</a></li>
+        </ul>
       </div>
 
       <div className="footer-section">
-        <h4>Contact</h4>
-        <p>Email: info@womenindatascience.com</p>
+        <h3>Contact</h3>
+        <p>Email: wids@em-lyon.com</p>
       </div>
     </div>
   </div>
